b44d4aab526b00830702b04454baf8a0dab8338f
CEJSH <eun2763@naver.com>
2025-01-11 19:59:49 +0900

    fix: input placeholder

--- src/components/UploadProfileForm.tsx
+++ src/components/UploadProfileForm.tsx
@@ -40,7 +40,7 @@ export default function UploadProfileForm() {
       <Input
         name={"name"}
         label="이름"
-        placeholder="이름을 입력해주세요"
+        placeholder="이름을 입력해주세요."
         onChange={handleFormValues}
         value={formValues.name}
         helpMessage={errors.name}
@@ -48,8 +48,8 @@ export default function UploadProfileForm() {
       <Spacing size={30} />
       <Input
         name={"lightyId"}
-        label="프로필 계정 아이디"
-        placeholder="프로필 계정 아이디를 입력해주세요"
+        label="계정 아이디"
+        placeholder="영문 소문자, 숫자, 특수기호 (_)만 입력 가능"
         onChange={handleFormValues}
         displayLength={15}
         value={formValues.lightyId}
@@ -57,7 +57,7 @@ export default function UploadProfileForm() {
       />
       <Spacing size={6} />
       <span className="text-C2 text-grayscale-500">
-        *영문 소문자, 숫자, 특수기호 (_)만 입력 가능
+        *계정 아이디는 프로필에 노출되며, 친구 추가 시 활용돼요!
       </span>
       <FixedBottomButton
         label="라이티 시작하기"
