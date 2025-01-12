2e2cf4a922ba1e96d82f0623ba053e5ad6b39144
CEJSH <eun2763@naver.com>
2025-01-12 17:13:27 +0900

    lint error fixed

--- src/components/gathering/hooks/useGatherings.ts
+++
@@ -1,6 +0,0 @@
-import { getGatherings } from "@/remote/gathering";
-import { useQuery } from "@tanstack/react-query";
-
-export default function useGatherings() {
-  return useQuery({ queryKey: ["gatherings"], queryFn: () => getGatherings() });
-}
--- src/components/groups/hooks/useMakeGroup.tsx
+++ src/components/groups/hooks/useMakeGroup.tsx
@@ -1,8 +1,12 @@
-import { GroupInfo } from "@/models/group";
+import * as lighty from "lighty-type";
 import { postGroup } from "@/remote/group";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useMakeGroup({ group }: { group: GroupInfo }) {
+export default function useMakeGroup({
+  group,
+}: {
+  group: lighty.CreateGroupRequest;
+}) {
   return useQuery({
     queryKey: ["make/group", group.name],
     queryFn: () => postGroup({ group }),
