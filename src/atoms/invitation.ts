3b3114ce61e4fe49fa2fab8d221339fe1aa1a0e9
CEJSH <eun2763@naver.com>
2025-01-20 08:50:59 +0900

    fix lint err

--- src/atoms/invitation.ts
+++ src/atoms/invitation.ts
@@ -1,7 +1,8 @@
 import { atom } from "recoil";
 import * as lighty from "lighty-type";
+import { GatheringInvitation } from "@/models/gathering";
 
-export const selectedInvitationAtom = atom<lighty.GatheringInvitation | null>({
+export const selectedInvitationAtom = atom<GatheringInvitation | null>({
   key: "invitation/selectedInvitation",
   default: null,
 });
