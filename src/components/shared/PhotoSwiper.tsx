4d2ccecfc8ada388a3d9a3b5748dfa0b61a2646f
CEJSH <eun2763@naver.com>
2025-02-26 13:56:58 +0900

    fix lazy to eager

--- src/components/shared/PhotoSwiper.tsx
+++ src/components/shared/PhotoSwiper.tsx
@@ -55,7 +55,7 @@ const PhotoSwiper = memo(
           >
             {loaded === false && <DotSpinner />}
             <Image
-              loading="lazy"
+              loading="eager"
               src={image || Lighty}
               alt={`Feed image ${idx + 1}`}
               className={styles.image}
