94dfea158aca885f605f36c6fd81b6b33670c66a
CEJSH <eun2763@naver.com>
2025-03-05 03:07:59 +0900

    refactor: social page

--- src/components/social/FriendsAndGroups.tsx
+++ src/components/social/FriendsAndGroups.tsx
@@ -1,133 +1,48 @@
 "use client";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
+import dynamic from "next/dynamic";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useRecoilState, useRecoilValue } from "recoil";
+
 import ArrowRightIcon from "@/components/shared/Icon/ArrowRightIcon";
-import * as lighty from "lighty-type";
 import Spacing from "../shared/Spacing";
-import UserFriendsListContainer from "@/components/friends/UserFriendsListContainer";
-import useDebounce from "@/hooks/debounce";
-import SearchedFriendsListContainer from "@/components/friends/SearchedFriendsListContainer";
-import Groups from "@/components/groups/Group";
 import Flex from "@/components/shared/Flex";
 import Button from "@/components/shared/Button/Button";
-import Panel, { PanelLength } from "@/components/shared/Panel/Panel";
+import Panel from "@/components/shared/Panel/Panel";
 import SearchInput from "@/components/shared/Input/SearchBar";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { friendSearchAtom, friendsSelectedTabAtom } from "@/atoms/friends";
+import FullPageLoader from "../shared/FullPageLoader";
+
+import useDebounce from "@/hooks/debounce";
+import useInfiniteScroll from "@/hooks/useInfiniteScroll";
 import { useAuth } from "../shared/providers/AuthProvider";
 import useFriends from "../friends/hooks/useFriends";
-import useInfiniteScroll from "@/hooks/useInfiniteScroll";
-import FullPageLoader from "../shared/FullPageLoader";
-import { useRouter, useSearchParams } from "next/navigation";
 import useFriendsRequestTotalCount from "../friends/hooks/useFriendsRequestCount";
+import { friendSearchAtom, friendsSelectedTabAtom } from "@/atoms/friends";
 
-const TabSection = ({
-  selectedTab,
-  setSelectedTab,
-}: {
-  selectedTab: "1" | "2";
-  setSelectedTab: (tabName: "1" | "2") => void;
-}) => {
-  const PanelProps = {
-    title1: "친구",
-    title2: "그룹",
-    long: "short" as PanelLength,
-    selectedTab,
-    onClick: setSelectedTab,
-  };
+const UserFriendsListContainer = dynamic(
+  () => import("@/components/friends/UserFriendsListContainer"),
+  { ssr: false, loading: () => <FullPageLoader /> }
+);
 
-  return (
-    <div className="px-5 pt-12">
-      <Panel {...PanelProps} year={false} />
-      <Spacing size={20} />
-    </div>
-  );
-};
+const SearchedFriendsListContainer = dynamic(
+  () => import("@/components/friends/SearchedFriendsListContainer"),
+  { ssr: false, loading: () => <FullPageLoader /> }
+);
 
-const TabContent = ({
-  selectedTab,
-  debouncedSearch,
-  friends,
-}: {
-  selectedTab: string;
-  debouncedSearch: string;
-  friends: lighty.User[];
-}) => {
-  return (
-    <>
-      <div style={{ display: selectedTab === "1" ? "block" : "none" }}>
-        {debouncedSearch.length > 0 ? (
-          <SearchedFriendsListContainer debouncedSearch={debouncedSearch} />
-        ) : (
-          friends && <UserFriendsListContainer friends={friends} />
-        )}
-      </div>
-      <div style={{ display: selectedTab === "2" ? "block" : "none" }}>
-        <Groups />
-      </div>
-    </>
-  );
-};
-
-const FriendListSection = ({
-  friends,
-  onAddFriend,
-  onNavigateToFriends,
-  requestCount,
-}: {
-  friends: lighty.User[];
-  onAddFriend: () => void;
-  onNavigateToFriends: () => void;
-  requestCount: { count: number };
-}) => {
-  return (
-    <>
-      <Flex
-        direction="column"
-        justify="space-between"
-        align="center"
-        className="px-5 gap-4"
-      >
-        <Flex justify="space-between" align="center" className="w-full">
-          <span
-            className="text-T4"
-            id="friendList"
-          >{`친구 ${friends.length}`}</span>
-          <Button className={styles.button} onMouseDown={onAddFriend}>
-            친구 추가
-          </Button>
-        </Flex>
-        <SearchInput
-          type="friends"
-          className="!bg-grayscale-50"
-          placeholder="이름/아이디로 검색하기"
-        />
-        <li className={styles.request} onMouseDown={onNavigateToFriends}>
-          <span>
-            {`요청`}
-            <span className="text-[#FA6767] ml-1">{`${requestCount.count}`}</span>
-          </span>
-          <ArrowRightIcon />
-        </li>
-      </Flex>
-      <Spacing size={16} />
-    </>
-  );
-};
-
-const styles = {
-  button:
-    "py-2 px-3 bg-grayscale-50 text-T6 rounded-[8px] hover:scale-105 transition-transform cursor-pointer hover:bg-grayscale-100",
-  request:
-    "text-T5 w-full flex py-5 px-6 rounded-[20px] items-center cursor-pointer border border-grayscale-100 justify-between alien-center hover:bg-grayscale-50",
-};
+const Groups = dynamic(() => import("@/components/groups/Group"), {
+  ssr: false,
+  loading: () => <FullPageLoader />,
+});
 
 export default function FriendsAndGroups() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { userInfo } = useAuth();
+
   const [selectedTab, setSelectedTab] = useRecoilState(friendsSelectedTabAtom);
   const search = useRecoilValue(friendSearchAtom);
-  const searchParams = useSearchParams();
   const debouncedSearch = useDebounce(search);
-  const router = useRouter();
-  const { userInfo } = useAuth();
+
   const {
     data: friends = [],
     loadMore,
@@ -135,26 +50,36 @@ export default function FriendsAndGroups() {
   } = useFriends({
     userId: userInfo?.accountId || "",
   });
+
   const { data: requestCount = { count: 0 } } = useFriendsRequestTotalCount();
+
+  useInfiniteScroll({ isFetching, loadMore });
 
   useEffect(() => {
     const tabParam = searchParams?.get("tab");
-    if (tabParam === "group") {
-      setSelectedTab("2");
-    } else {
-      setSelectedTab("1");
+    setSelectedTab(tabParam === "group" ? "2" : "1");
+  }, [searchParams, setSelectedTab]);
+
+  const handleAddFriend = useCallback(
+    () => router.push("/friends/search"),
+    [router]
+  );
+
+  const handleNavigateToFriends = useCallback(
+    () => router.push("/friends"),
+    [router]
+  );
+
+  const renderTabContent = useMemo(() => {
+    if (selectedTab === "1") {
+      return debouncedSearch.length > 0 ? (
+        <SearchedFriendsListContainer debouncedSearch={debouncedSearch} />
+      ) : (
+        <UserFriendsListContainer friends={friends} />
+      );
     }
-  }, [searchParams]);
-
-  useInfiniteScroll({ isFetching: isFetching, loadMore });
-
-  const handleAddFriend = useCallback(() => {
-    router.push("/friends/search");
-  }, [router]);
-
-  const handleNavigateToFriends = useCallback(() => {
-    router.push("/friends");
-  }, [router]);
+    return <Groups />;
+  }, [selectedTab, debouncedSearch, friends]);
 
   if (isFetching) {
     return <FullPageLoader />;
@@ -162,20 +87,56 @@ export default function FriendsAndGroups() {
 
   return (
     <>
-      <TabSection selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      {selectedTab !== "2" && (
-        <FriendListSection
-          friends={friends}
-          onAddFriend={handleAddFriend}
-          requestCount={requestCount}
-          onNavigateToFriends={handleNavigateToFriends}
+      <div className="px-5 pt-12">
+        <Panel
+          title1="친구"
+          title2="그룹"
+          long="short"
+          selectedTab={selectedTab}
+          onClick={setSelectedTab}
+          year={false}
         />
+        <Spacing size={20} />
+      </div>
+
+      {selectedTab !== "2" && (
+        <Flex
+          direction="column"
+          justify="space-between"
+          align="center"
+          className="px-5 gap-4"
+        >
+          <Flex justify="space-between" align="center" className="w-full">
+            <span className="text-T4" id="friendList">
+              {`친구 ${friends.length}`}
+            </span>
+            <Button
+              className="py-2 px-3 bg-grayscale-50 text-T6 rounded-[8px] hover:scale-105 transition-transform cursor-pointer hover:bg-grayscale-100"
+              onMouseDown={handleAddFriend}
+            >
+              친구 추가
+            </Button>
+          </Flex>
+
+          <SearchInput
+            type="friends"
+            className="!bg-grayscale-50"
+            placeholder="이름/아이디로 검색하기"
+          />
+
+          <li
+            className="text-T5 w-full flex py-5 px-6 rounded-[20px] items-center cursor-pointer border border-grayscale-100 justify-between alien-center hover:bg-grayscale-50"
+            onMouseDown={handleNavigateToFriends}
+          >
+            <span>
+              {`요청`}
+              <span className="text-[#FA6767] ml-1">{`${requestCount.count}`}</span>
+            </span>
+            <ArrowRightIcon />
+          </li>
+        </Flex>
       )}
-      <TabContent
-        selectedTab={selectedTab}
-        debouncedSearch={debouncedSearch}
-        friends={friends}
-      />
+      {renderTabContent}
     </>
   );
 }
