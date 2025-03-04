c8993056ab97117fa082c5850857c9da8f53d324
CEJSH <eun2763@naver.com>
2025-03-05 02:41:28 +0900

    fix lint error

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -2,12 +2,11 @@ import FriendsPageHeader from "@/components/friends/FriendsPageHeader";
 import clsx from "clsx";
 import SentReceivedFriendRequestsList from "@/components/friends/SentReceivedFriendRequestsList";
 import { Suspense } from "react";
-import DotSpinner from "@/components/shared/Spinner/DotSpinner";
 
 export default function FriendsPage() {
   return (
     <div className="bg-grayscale-50 h-full">
-      <Suspense fallback={<DotSpinner />}>
+      <Suspense fallback={<div>로딩중</div>}>
         <div className={clsx("max-w-[430px] fixed w-full z-1 bg-grayscale-50")}>
           <FriendsPageHeader label="친구 요청" />
         </div>
