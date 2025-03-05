e26c321efbe516bb9a6176bbce56b1e0b6738b15
CEJSH <eun2763@naver.com>
2025-03-05 21:31:41 +0900

    fix frame address

--- src/components/cards/DecorateWithStickers.tsx
+++ src/components/cards/DecorateWithStickers.tsx
@@ -42,6 +42,9 @@ export default function DecorateWithStickers() {
     "https://cdn.lighty.today/frame5.jpeg",
     "https://cdn.lighty.today/frame6.jpeg",
     "https://cdn.lighty.today/frame7.jpeg",
+    "https://cdn.lighty.today/frame8.png",
+    "https://cdn.lighty.today/frame9.png",
+    "https://cdn.lighty.today/frame10.png",
   ];
 
   useEffect(() => {
