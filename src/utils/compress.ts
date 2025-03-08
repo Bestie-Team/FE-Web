a31f764aec6bbda2e23d59a3968ffa1615199d57
CEJSH <eun2763@naver.com>
2025-03-08 17:35:45 +0900

    fix: fileType

--- src/utils/compress.ts
+++ src/utils/compress.ts
@@ -6,7 +6,6 @@ export const compressImage = async (file: File, maxWidth?: number) => {
     maxSizeMB: 5,
     maxWidthOrHeight: maxWidth ? maxWidth : 500,
     useWebWorker: true,
-    fileType: "image/webp",
   };
   try {
     const compressedFile = await imageCompression(imageFile, options);
