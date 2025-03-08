22941b7d71ec4da4b70d09404871987039eaea9a
CEJSH <eun2763@naver.com>
2025-03-08 17:45:43 +0900

    fix: compress option

--- src/utils/compress.ts
+++ src/utils/compress.ts
@@ -4,8 +4,9 @@ export const compressImage = async (file: File, maxWidth?: number) => {
   const imageFile = file;
   const options = {
     maxSizeMB: 5,
-    maxWidthOrHeight: maxWidth ? maxWidth : 500,
+    maxWidthOrHeight: maxWidth ? maxWidth : 1200,
     useWebWorker: true,
+    quality: 0.9,
   };
   try {
     const compressedFile = await imageCompression(imageFile, options);
