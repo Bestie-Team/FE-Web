209a05ff77d5a375a4e3d78abe6143286ee2efc3
Haesung <koh1260@naver.com>
2025-03-11 03:21:56 +0900

    feat: add safe area inset to bottom nav

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -4,6 +4,7 @@ import { useActiveNavigation } from "@/hooks/useActiveNavigation";
 import { NavLink } from "./NavBar/NavLink";
 import useUserProfile from "../users/hooks/useUserProfile";
 import dynamic from "next/dynamic";
+import clsx from "clsx";
 
 const FloatingButton = dynamic(() => import("./Button/FloatingButton"), {
   ssr: false,
@@ -66,11 +67,14 @@ const NavBar = () => {
   return (
     <nav
       style={{ zIndex: 99 }}
-      className={`
+      className={clsx(
+        `
         fixed left-0 right-0 bottom-0 bg-base-white w-full max-w-[430px]
         flex justify-between px-3 pt-1 pb-2 mx-auto
         border-t border-grayscale-10
-      `}
+      `,
+        window.ReactNativeWebView ? "pb-safe-bottom" : ""
+      )}
     >
       {items}
       {shouldShowFloatingButton && <FloatingButton tooltip={tooltip} />}
