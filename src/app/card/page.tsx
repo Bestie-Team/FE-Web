5d8ca9acc28453ce0101069acfd48114732134f8
CEJSH <eun2763@naver.com>
2025-03-11 17:40:49 +0900

    fix lint error

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 import DotSpinner from "@/components/shared/Spinner/DotSpinner";
 import dynamic from "next/dynamic";
 import FramePageSkeleton from "@/components/shared/Skeleton/FramePageSkeleton";
-import CardPageSkeleton from "@/components/shared/Skeleton/CardPageSkeleton";
 
 const ChooseFrame = dynamic(() => import("@/components/cards/ChooseFrame"), {
   ssr: false,
@@ -32,7 +31,6 @@ export default function Page() {
       return "프레임 선택";
     } else return "포토 카드";
   };
-  return <CardPageSkeleton />;
 
   return (
     <div className="bg-base-white h-dvh overflow-y-scroll no-scrollbar">
