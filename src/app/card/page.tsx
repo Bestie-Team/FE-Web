6fda64879ecbc46aa0a5f0f66d67b7e572c1fa54
CEJSH <eun2763@naver.com>
2025-03-25 01:35:53 +0900

    fix: remove dynamic loading to card component

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -2,20 +2,15 @@
 import React, { useState } from "react";
 import ChoosingGatheringToDecorate from "@/components/cards/ChoosingGatheringToDecorate";
 import { useRouter } from "next/navigation";
-import DotSpinner from "@/components/shared/Spinner/DotSpinner";
 import dynamic from "next/dynamic";
 import FramePageSkeleton from "@/components/shared/Skeleton/FramePageSkeleton";
 import HeaderWithBtn from "@/components/shared/Header/HeaderWithBtn";
+import DecorateWithStickers from "@/components/cards/DecorateWithStickers";
 
 const ChooseFrame = dynamic(() => import("@/components/cards/ChooseFrame"), {
   ssr: false,
   loading: () => <FramePageSkeleton />,
 });
-
-const DecorateWithStickers = dynamic(
-  () => import("@/components/cards/DecorateWithStickers"),
-  { ssr: false, loading: () => <DotSpinner /> }
-);
 
 export default function Page() {
   const [step, setStep] = useState<number>(1);
